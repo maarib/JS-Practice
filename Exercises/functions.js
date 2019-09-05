c5621b8c63ec721c83d02d61504ca97c57f85daf
Maaribou <iammaarib@gmail.com>
2019-09-05 13:42:01 -0400

    Recursive exercises added

--- Exercises/functions.js
+++ Exercises/functions.js
@@ -75,19 +75,3 @@ console.log(wrap2());
 console.log(wrap1());
 
 */
-
-// Recursive Functions
-function findSolution (target){
-    function find(current,history){
-        if (current == target){
-            return history;
-        }
-        else if (current > target){
-            return null;
-        }else {
-            return find(current + 5, '${history}' + '+ 5') || find(current * 3,'(${history} * 3)');
-        }
-    }
-    return find(1,'1');
-}
-document.write(findSolution(24));
